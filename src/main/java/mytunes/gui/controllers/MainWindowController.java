f3dfe7714f0bfff4a57747791f9c55b6aea8594d
Patrik Valentiny <patrikvalentiny@gmail.com>
2022-11-28 11:02:25 +0100

    playPause button created

--- src/main/java/mytunes/gui/controllers/MainWindowController.java
+++ src/main/java/mytunes/gui/controllers/MainWindowController.java
@@ -1,10 +1,32 @@
 package mytunes.gui.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import mytunes.MyTunes;
+
+import java.util.Objects;
 
 public class MainWindowController {
 
+    @FXML
+    private ImageView playPause;
+    private boolean isPlaying = false;
+
+
     public void onPlaylistNewButton(ActionEvent actionEvent) {
         System.out.println("New playlist button clicked");
+    }
+
+    public void playPauseMouseDown(MouseEvent mouseEvent) {
+        System.out.println("Play/Pause button mouse down");
+        if (!isPlaying) {
+            playPause.setImage(new Image(Objects.requireNonNull(MyTunes.class.getResourceAsStream("images/pause.png"))));
+        } else {
+            playPause.setImage(new Image(Objects.requireNonNull(MyTunes.class.getResourceAsStream("images/play.png"))));
+        }
+        isPlaying = !isPlaying;
     }
 }
