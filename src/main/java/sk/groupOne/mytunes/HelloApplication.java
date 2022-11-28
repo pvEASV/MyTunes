6d53ff660ae867399659c645251314c980773b6a
Julia Ilasova <you@example.com>
2022-11-28 09:28:36 +0100

    Added a main window

--- src/main/java/sk/groupOne/mytunes/HelloApplication.java
+++ src/main/java/sk/groupOne/mytunes/HelloApplication.java
@@ -10,10 +10,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Branch test!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/MyTunesMainWindow.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("MyTunes");
+        stage.setResizable(false);
         stage.setScene(scene);
+        stage.centerOnScreen();
         stage.show();
     }
 
