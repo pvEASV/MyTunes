535f0c2c41c57c706b60cd2b66352556b1fd4f43
Julia Ilasova <you@example.com>
2022-11-29 12:36:11 +0100

    new song - choose a file path implemented

--- src/main/java/mytunes/dal/SongDAO.java
+++ src/main/java/mytunes/dal/SongDAO.java
@@ -5,6 +5,7 @@ import mytunes.be.Song;
 public class SongDAO {
 
     public Song createSong(String title, String filepath) {
+        System.out.println("success");
         return new Song(title, filepath);
     }
 
