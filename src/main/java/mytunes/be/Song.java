1fa8eedd2dc8442372f0e6f98343f059eaee450c
Patrik Valentiny <patrikvalentiny@gmail.com>
2022-11-29 12:07:18 +0100

    adding getters and setters + constructor

--- src/main/java/mytunes/be/Song.java
+++ src/main/java/mytunes/be/Song.java
@@ -8,11 +8,54 @@ public class Song {
     private String path;
     private Genre genre;
 
-    public Song(int id, String title, Author author, String path, Genre genre) {
-        this.id = id;
+    public Song(String title, Author author, String path, Genre genre, int duration) {
         this.title = title;
         this.author = author;
         this.path = path;
         this.genre = genre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
     }
 }
